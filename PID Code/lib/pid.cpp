50725c68529b20490bf1d21c3063caebc5f66b2e
ChayceJRoss <chayceross108@gmail.com>
2023-06-08 09:01:15 -0700

    Fixed some PID code

--- PID Code/lib/pid.cpp
+++ PID Code/lib/pid.cpp
@@ -1,3 +1,13 @@
+/*
+current: current reading from sensors
+target: desired reading from sensors
+previous: the last reading from the sensors
+elapsed_time: time between last pid execution and current excecution
+kp: proportional coefficient
+ki: integral coefficient
+kd: derivatice coefficient
+imax: limit for anti-windup
+*/
 int pid(double current, double target, double previous, double integral, int elapsed_time, double kp, double ki, double kd, double imax) {
     double error = target - current;
     double p = kp * error;
@@ -7,7 +17,7 @@ int pid(double current, double target, double previous, double integral, int ela
     } else if (i < (-1 * imax)) {
         i = -1 * imax;
     }
-    double d = kd * (error - previous) / elapsed_time;
+    double d = kd * (error - previous) / (double) elapsed_time;
     return p + i + d;
 }
 
--- PID Code/src/main.cpp
+++ PID Code/src/main.cpp
@@ -9,6 +9,9 @@
 
 #define INPUT_PIN_LEFT PA0
 #define INPUT_PIN_RIGHT PA1
+#define INPUT_P_PIN PA2
+#define INPUT_D_PIN PA3
+#define INPUT_I_PIN PA4
 
 int time0;
 int time1;
@@ -18,11 +21,17 @@ int dt;
 int pid_output;
 int input_reading;
 int previous;
+int kp;
+int ki;
+int kd;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(INPUT_PIN_LEFT, INPUT);
   pinMode(INPUT_PIN_RIGHT, INPUT);
+  pinMode(INPUT_P_PIN, INPUT);
+  pinMode(INPUT_D_PIN, INPUT);
+  pinMode(INPUT_I_PIN, INPUT);
   Serial.begin(9600);
   time0 = millis();
   time1 = millis();
@@ -32,14 +41,17 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   time0 = millis();
   dt = time1 - time0;
   time1 = time0;
+  kp = analogRead(INPUT_P_PIN);
+  ki = analogRead(INPUT_I_PIN);
+  kd = analogRead(INPUT_D_PIN);
   left = digitalRead(INPUT_PIN_LEFT);
   right = digitalRead(INPUT_PIN_RIGHT);
   input_reading = left - right;
-  pid_output = pid(input_reading, 0, previous, 0, dt, KP, KI, KD, IMAX);
+  pid_output = pid(input_reading, 0, previous, 0, dt, kp, ki, kd, IMAX);
   activate_motor(pid_output);
   previous = input_reading;
 }
